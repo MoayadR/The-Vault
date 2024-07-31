9643b2534b77c52abbe3b78d687562a82be0264b
MoayadR <moayad.r.abdalraouf@gmail.com>
2024-07-31 14:36:44 +0300

    Making the first screen

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, screen } = require('electron')
+const path = require('node:path')
+
 
 const createWindow = (winPath) => {
+    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
     const win = new BrowserWindow({
-        width: 800,
-        height: 600
+        width, height,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     })
 
     win.loadFile(winPath)
 }
 
 app.whenReady().then(() => {
-    createWindow('pages/index.html')
+    createWindow('pages/index/index.html')
 
     app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow('pages/index.html')
+        if (BrowserWindow.getAllWindows().length === 0) createWindow('pages/index/index.html')
     })
 })
 
