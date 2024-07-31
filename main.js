ee7ef92c25ea73ac79a73956bbb12b923edbc55d
MoayadR <moayad.r.abdalraouf@gmail.com>
2024-07-31 16:58:45 +0300

    Finished Initial Page

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ const { app, BrowserWindow, screen } = require('electron')
 const path = require('node:path')
 
 
-const createWindow = (winPath) => {
+const createWindow = (winPath, preloadPath) => {
     const { width, height } = screen.getPrimaryDisplay().workAreaSize;
     const win = new BrowserWindow({
         width, height,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+            preload: path.join(__dirname, preloadPath)
         }
     })
 
@@ -15,10 +15,10 @@ const createWindow = (winPath) => {
 }
 
 app.whenReady().then(() => {
-    createWindow('pages/index/index.html')
+    createWindow('pages/index/index.html', 'preload.js')
 
     app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow('pages/index/index.html')
+        if (BrowserWindow.getAllWindows().length === 0) createWindow('pages/index/index.html', 'preload.js')
     })
 })
 
